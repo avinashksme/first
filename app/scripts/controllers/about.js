9f83a8f00e2dbf62ff89f176082071d673c8ed63
Avinash Singh <avinash.s@rentomojo.com>
2016-07-03 02:52:51 +0530

    added team to about

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,6 +8,67 @@
  * Controller of the rmwebclientApp
  */
 angular.module('testApp')
-  .controller('AboutCtrl',[function () {
-
+  .controller('AboutCtrl',['$scope', function ($scope) {
+    $scope.teamMembers = [
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      },
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      },
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      },
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      },
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      },
+      {
+          team: 'technology',
+          photo: 'images/avinash_singh.jpg',
+          name: 'Avinash Singh',
+          position: 'Frontend Developer + c0d3Genie();',
+          facebookUrl: 'https://www.facebook.com/avinashks.me',
+          twitterUrl: 'https://twitter.com/play_avi',
+          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
+          instagramUrl: 'https://www.instagram.com/avinash.ks/'
+      }
+    ];
 }]);
