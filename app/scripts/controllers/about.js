a1869a88011cfa771808bec44a89770a750cabe0
Avinash Singh <avinash.s@rentomojo.com>
2016-07-18 18:26:26 +0530

    serving data from json

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,69 +8,11 @@
  * Controller of the rmwebclientApp
  */
 angular.module('testApp')
-  .controller('AboutCtrl',['$scope', function ($scope) {
-    $scope.teamMembers = [
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      },
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      },
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      },
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      },
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      },
-      {
-          team: 'technology',
-          photo: 'images/avinash_singh.jpg',
-          name: 'Avinash Singh',
-          position: 'Frontend Developer + c0d3Genie();',
-          facebookUrl: 'https://www.facebook.com/avinashks.me',
-          twitterUrl: 'https://twitter.com/play_avi',
-          linkedinUrl: 'https://www.linkedin.com/in/mravinash',
-          instagramUrl: 'https://www.instagram.com/avinash.ks/'
-      }
-    ];
+  .controller('AboutCtrl',['$scope', '$http', function ($scope, $http) {
+    $http.get('/team.json').success(function(data){
+      console.log(data);
+      $scope.teamMembers = data;
+    })
 
     $scope.open = function(){
       var modalInstance = $uibModal.open({
